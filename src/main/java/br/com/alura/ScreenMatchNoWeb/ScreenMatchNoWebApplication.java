c0d470781f9ca0a696867f409393bac3dd324343
TatianaSchiBer <tatianaschiavuzzobernardi@gmail.com>
2024-05-06 20:01:04 +0200

    update

--- src/main/java/br/com/alura/ScreenMatchNoWeb/ScreenMatchNoWebApplication.java
+++ src/main/java/br/com/alura/ScreenMatchNoWeb/ScreenMatchNoWebApplication.java
@@ -1,10 +1,15 @@
 package br.com.alura.ScreenMatchNoWeb;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.alura.ScreenMatchNoWeb.model.DadosEpisodio;
 import br.com.alura.ScreenMatchNoWeb.model.DadosSerie;
+import br.com.alura.ScreenMatchNoWeb.model.DadosTemporada;
 import br.com.alura.ScreenMatchNoWeb.service.ConsumoApi;
 import br.com.alura.ScreenMatchNoWeb.service.ConverteDados;
 
@@ -27,7 +32,20 @@ public class ScreenMatchNoWebApplication implements CommandLineRunner{
 		    DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
 		    System.out.println(dados);
 		    
+		 
+
+		    json = consumoApi.obterDados("https://omdbapi.com/?t=gilmore+girls&season=1&episode=2&apikey=6585022c");
+		    DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
+		    System.out.println(dadosEpisodio);
 		    
+		    List<DadosTemporada> temporadas = new ArrayList<DadosTemporada>();
+
+		    for(int i=1; i<=dados.totalTemporadas(); i++) {
+		    	  json = consumoApi.obterDados("https://omdbapi.com/?t=gilmore+girls&season=" + i + "&apikey=6585022c");
+		    	  DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+		    	  temporadas.add(dadosTemporada);
+		    }
+		    temporadas.forEach(System.out::println);
 	}
 
 }
