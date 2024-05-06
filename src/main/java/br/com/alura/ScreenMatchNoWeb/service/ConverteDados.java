868eae0fdd265f0e3c3fd95017153cfe0d5490a1
TatianaSchiBer <tatianaschiavuzzobernardi@gmail.com>
2024-05-06 18:38:38 +0200

    Project Start

---
+++ src/main/java/br/com/alura/ScreenMatchNoWeb/service/ConverteDados.java
@@ -0,0 +1,24 @@
+package br.com.alura.ScreenMatchNoWeb.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class ConverteDados implements IConverteDados {
+	
+	private ObjectMapper mapper = new ObjectMapper();
+
+	@Override
+	public <T> T obterDados(String json, Class<T> classe) {
+		// TODO Auto-generated method stub
+		try {
+			return mapper.readValue(json, classe);
+		
+		} catch (JsonProcessingException e) {
+			throw new RuntimeException(e);
+		}
+	}
+    
+   
+    
+    }
+
---
+++ src/test/java/br/com/alura/ScreenMatchNoWeb/ScreenMatchNoWebApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.ScreenMatchNoWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreenMatchNoWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
